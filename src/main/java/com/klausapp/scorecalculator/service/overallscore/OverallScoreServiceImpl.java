dc41b66475fa987fdef0223afb6984e0fd0b5a48
Martin <mkelpman@gmail.com>
2024-04-01 15:13:48 +0300

    Implement an overall score gRPC service

---
+++ src/main/java/com/klausapp/scorecalculator/service/overallscore/OverallScoreServiceImpl.java
@@ -0,0 +1,27 @@
+package com.klausapp.scorecalculator.service.overallscore;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.time.LocalDate;
+
+@GrpcService
+public class OverallScoreServiceImpl extends OverallScoreServiceGrpc.OverallScoreServiceImplBase {
+
+    private final OverallScoreCalculator overallScoreCalculator;
+
+    public OverallScoreServiceImpl(OverallScoreCalculator overallScoreCalculator) {
+        this.overallScoreCalculator = overallScoreCalculator;
+    }
+
+    @Override
+    public void getOverallScore(OverallScoreRequest request, StreamObserver<OverallScoreResponse> responseObserver) {
+        int score = overallScoreCalculator
+                .calculateOverallScoreForPeriod(
+                        LocalDate.parse(request.getPeriodStartDate()), LocalDate.parse(request.getPeriodEndDate()));
+        OverallScoreResponse response = OverallScoreResponse.newBuilder().setScore(score).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+}
